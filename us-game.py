abe32548ac263147b6435e559915338db0f70114
Bojan Stanojoski <93086902+bopojapann@users.noreply.github.com>
2021-12-10 15:46:15 +0100

    Add files via upload

---
+++ us-game.py
@@ -0,0 +1,37 @@
+import turtle
+
+import pandas
+
+screen =  turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+pras = pandas.read_csv("50_states.csv")
+lis = pras["state"].to_list()
+print(pras)
+
+
+def mouse(x, y):
+    print(x, y)
+turtle.onscreenclick(mouse)
+
+game_on = []
+while len(game_on) < 45:
+
+    answer = screen.textinput(title="Ques the state", prompt="What another state name?")
+    if answer == "exit":
+        break
+    if answer in lis:
+        game_on.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = pras[pras["state"] == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        b = pras[pras["state"] == answer]
+        t.write(answer)
+        print(b)
+turtle.mainloop()
+
